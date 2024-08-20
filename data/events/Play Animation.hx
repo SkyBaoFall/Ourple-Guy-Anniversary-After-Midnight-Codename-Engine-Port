eaaebbddf851c00e7a612cd74eec758394fb0eca
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2024-08-20 07:37:18 -0700

    like what the fuck github??

--- data/events/Play Animation.hx
+++
@@ -1,6 +0,0 @@
-function onEvent(eventEvent) {
-    if (eventEvent.event.name == "Play Animation") {
-        for (char in strumLines.members[eventEvent.event.params[1]].characters)
-            char.playAnim(eventEvent.event.params[0], true, null);
-    }
-}
