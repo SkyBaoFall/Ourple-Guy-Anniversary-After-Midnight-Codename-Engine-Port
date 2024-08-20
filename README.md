a39a642cc95234c6ea8d54d67298c455a37041ee
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2024-08-20 07:55:55 -0700

    killing myself

--- README.md
+++
@@ -1,2 +0,0 @@
-# Cuz Yes
-`ourple guys no wayyyyyy`
--- data/events/Change Character.hx
+++
@@ -1,9 +0,0 @@
-function postCreate() {
-    for (event in PlayState.SONG.events)
-        if (event.name == "Change Character") preloadChar(event.params[0], event.params[1], event.params[2]);
-}
-
-function onEvent(_) {
-    var params:Array = _.event.params;
-    if (_.event.name == "Change Character") changeChar(params[0], params[1], params[2]);
-}
